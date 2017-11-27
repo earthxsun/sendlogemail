17a283ce1dd6c9d6f6474613cf389ca0ab69aa84
earthxsun <earthx@126.com>
2017-11-27 17:11:06 +0800

    modi ini files codes

--- readinifile.py
+++ readinifile.py
@@ -1,19 +1,20 @@
 import configparser
 
+
 def readconf():
     conf = configparser.ConfigParser()
     try:
-        conf.read('config.ini',encoding='UTF-8')
-        smtpserver = conf.get('config','smtp_server')
-        sender = conf.get('config','sender')
-        password = conf.get('config','password')
-        recipients = conf.get('config','recipients')
-        subject = conf.get('config','Subject')
-        mailcontent = conf.get('config','mailcontent')
+        conf.read('config.ini',encoding='GB2312')
+        smtpserver = conf.get('config', 'smtp_server')
+        sender = conf.get('config', 'sender')
+        password = conf.get('config', 'password')
+        recipients = conf.get('config', 'recipients')
+        subject = conf.get('config', 'Subject')
+        mailcontent = conf.get('config', 'mailcontent')
     except configparser.NoSectionError:
         print("没有找到配置文件：config.ini或者配置文件里没有config选项")
     else:
-        confdict = {'smtpsrv':smtpserver,'sender':sender,'pwd':password,'recipients':recipients,'subject':subject,
+        confdict = {'smtpsrv': smtpserver, 'sender':sender, 'pwd': password, 'recipients': recipients, 'subject': subject,
                     'mailcontent':mailcontent}
         return confdict
 
